f604e22a5202c35589b32212f6a3be13724c672c
fauresystems <34378695+fauresystems@users.noreply.github.com>
2020-03-01 09:01:16 +0100

    Removed .idea and __pychache__ folders from repository

---
+++ install-with-python38-venv.bat
@@ -0,0 +1,3 @@
+C:\"Program Files"\7-Zip\7z.exe a -ttar teletextplugin-%date:~6,4%%date:~3,2%%date:~0,2%.tar -xr!.git -xr!ready -xr!_SYNCAPP -xr!*.tgz -xr!*.vs -xr!*.pyc -xr!*.log.* -xr!.eric* -xr!_eric* -xr!__* ./
+;C:\"Program Files"\7-Zip\7z.exe a -tgzip teletextplugin-1.0-%date:~6,4%%date:~3,2%%date:~0,2%.tgz teletextplugin-%date:~6,4%%date:~3,2%%date:~0,2%.tar
+;del teletextplugin-%date:~6,4%%date:~3,2%%date:~0,2%.tar
