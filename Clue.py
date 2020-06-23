714afe0b40a70b465d0e087efd98b54ff608b0c6
fauresystems <34378695+fauresystems@users.noreply.github.com>
2020-06-23 16:53:37 +0200

    Copyright to Faure Systems

--- Clue.py
+++ Clue.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 Clue.py
-MIT License (c) Marie Faure <dev at faure dot systems>
+MIT License (c) Faure Systems <dev at faure dot systems>
 
 Class for a clue.
 """
--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 main.py
-MIT License (c) Marie Faure <dev at faure dot systems>
+MIT License (c) Faure Systems <dev at faure dot systems>
 
 Applet to control remotely a Raspberry running Teletext Props.
 
